18e244e4356778cec4ca7575b0cc805a7c3f2adf
plees <peishu.huang@gmail.com>
2016-06-10 21:50:49 +0800

    first commit

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Printf("hello, world\n")
+    fmt.Println("hello. Peishu")
+    /*ctrl+click on function to see the declaration*/
+
+}
